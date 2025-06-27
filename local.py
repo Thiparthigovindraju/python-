8632dd269dd20fbed70b909351caa0e4e5e7652b
Thiparthigovindraju <153574355+Thiparthigovindraju@users.noreply.github.com>
2025-06-27 10:03:59 +0530

    Add files via upload

---
+++ local.py
@@ -0,0 +1,13 @@
+name="SSMB"
+director="SSR"
+movie="SSMB29"
+def moviedetails():
+    name="ssmb"
+    director="ssr"
+    movie="ssmb29"
+    print(f"this is {name} and with {director} making movie named {movie}")
+moviedetails()
+print(f"this is {name} and with {director} making movie named {movie}")
+
+    
+    
---
+++ return.py
@@ -0,0 +1,9 @@
+name="Govind"
+age="21"
+def myfunction():
+    a=100
+    b=50
+    print(a+b)
+    return a+b,a*b,a/b
+result=myfunction()
+print(result)
